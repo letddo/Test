4f2b817b35fe4262fcc3596ab214b87f69e15efe
letddo <78413649+letddo@users.noreply.github.com>
2025-11-05 16:28:13 +0900

    Update config.js

--- styles/js/config.js
+++ styles/js/config.js
@@ -16,7 +16,7 @@ let charadex = {};
 /* ==================================================================== */
 charadex.site = {
   title: "Test",
-  url: "https://m4ch1n4097.github.io/Test/",
+  url: "https://letddo.github.io/Test/",
   description: `A tool for organizing small ARPGs and species.`
 }
 
